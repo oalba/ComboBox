f932e7b2963a9a49f4f865bf8d344f2df1557956
zubiri <zubiri@zubiri-VirtualBox.(none)>
2015-05-27 13:45:10 +0200

    ondo doa

---
+++ src/com/zubiri/ComboBox/ComboBox.java
@@ -0,0 +1,84 @@
+package com.zubiri.ComboBox;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.DefaultComboBoxModel;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JLabel;
+
+public class ComboBox extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ComboBox frame = new ComboBox();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ComboBox() {
+		super("Letras");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 234, 148);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"A", "B", "C", "D", "E", "F", "G", "H"}));
+		comboBox.setBounds(170, 12, 51, 25);
+		contentPane.add(comboBox);
+		
+		JButton btnOk = new JButton("OK");
+		btnOk.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				String letra = (String)comboBox.getSelectedItem();
+				System.out.println("Has seleccionado: "+letra);
+			}
+		});
+		btnOk.setBounds(12, 82, 82, 25);
+		contentPane.add(btnOk);
+		
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				comboBox.setSelectedIndex(0);
+				System.out.println("Operación cancelada.");
+			}
+		});
+		btnCancel.setBounds(139, 82, 82, 25);
+		contentPane.add(btnCancel);
+		
+		JLabel lblSeleccionaUnaLetra = new JLabel("Selecciona una letra:");
+		lblSeleccionaUnaLetra.setBounds(12, 17, 153, 15);
+		contentPane.add(lblSeleccionaUnaLetra);
+	}
+}
